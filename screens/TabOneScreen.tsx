0435d79489051036326fdaa9138e421cff8ff3bc
Sumeet Pareek <simpleandaudacious@gmail.com>
2022-01-20 08:26:19 -0500

    Simple button works

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,32 +4,38 @@ import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 
+import { Button, ThemeProvider } from "react-native-elements";
+
 export default function TabOneScreen({
   navigation,
 }: RootTabScreenProps<"TabOne">) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Random Number TODO</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
-      <Text>
-        TODO .. Add button here that displays a new random number between 0 and
-        100 everytime it is clicked.
-      </Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <Text>
-        TODO .. Create min and max text fields, update random number button to
-        show a value between them.
-      </Text>
-    </View>
+    <ThemeProvider>
+      <Button title="Hey!" />
+    </ThemeProvider>
+
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Random Number TODO</Text>
+    //   <View
+    //     style={styles.separator}
+    //     lightColor="#eee"
+    //     darkColor="rgba(255,255,255,0.1)"
+    //   />
+    //   {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
+    //   <Text>
+    //     TODO .. Add button here that displays a new random number between 0 and
+    //     100 everytime it is clicked.
+    //   </Text>
+    //   <View
+    //     style={styles.separator}
+    //     lightColor="#eee"
+    //     darkColor="rgba(255,255,255,0.1)"
+    //   />
+    //   <Text>
+    //     TODO .. Create min and max text fields, update random number button to
+    //     show a value between them.
+    //   </Text>
+    // </View>
   );
 }
 
