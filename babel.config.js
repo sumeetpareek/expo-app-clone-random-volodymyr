573a72cc7f0c1d833d2e8056bc3b36315b5ab916
Sumeet Pareek <simpleandaudacious@gmail.com>
2022-01-20 09:04:55 -0500

    Uses element ui components

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin']
   };
 };
+// See: https://github.com/software-mansion/react-native-reanimated/issues/1875
