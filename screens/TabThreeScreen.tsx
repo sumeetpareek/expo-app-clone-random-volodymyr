b486ef0e5b4f483ccd32a7adf17ba25ffdc23f25
Sumeet Pareek <simpleandaudacious@gmail.com>
2022-01-23 21:21:41 -0500

    Arranges dice faces with flexWrap and alignContent

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,28 +1,77 @@
+import * as React from "react";
+
 import { StyleSheet } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
+import { Button, ThemeProvider } from "react-native-elements";
+
 export default function TabThreeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Dice Roll</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      {/* <EditScreenInfo path="/screens/TabThreeScreen.tsx" /> */}
-      <Text>TODO .. Add dice roll here</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.outputContainer}>
+        <View style={styles.diceFace}>
+          <Text>1</Text>
+        </View>
+        <View style={styles.diceFace}>
+          <Text>2</Text>
+        </View>
+        <View style={styles.diceFace}>
+          <Text>3</Text>
+        </View>
+        <View style={styles.diceFace}>
+          <Text>4</Text>
+        </View>
+        <View style={styles.diceFace}>
+          <Text>5</Text>
+        </View>
+        <View style={styles.diceFace}>
+          <Text>6</Text>
+        </View>
+      </View>
+      <View style={styles.controlsContainer}>
+        <Button
+          title="ROLL THE DICE"
+          onPress={() => {
+            alert("hey");
+          }}
+          buttonStyle={styles.button}
+          containerStyle={{ margin: 0 }}
+          titleStyle={{
+            color: "#fff",
+            marginHorizontal: 20,
+            fontWeight: "bold",
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end",
     alignItems: "center",
+    padding: 10,
+  },
+  outputContainer: {
+    flex: 4,
+    flexDirection: "row",
     justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    flexWrap: "wrap",
+    width: "100%",
+  },
+  controlsContainer: {
+    flex: 2,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
   },
   title: {
     fontSize: 20,
@@ -33,4 +82,11 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
+  diceFace: {
+    width: "26%",
+    height: 100,
+    margin: 4,
+    backgroundColor: "rgba(214, 61, 57, 1)",
+  },
+  button: { backgroundColor: "rgba(214, 61, 57, 1)", width: "100%" },
 });
